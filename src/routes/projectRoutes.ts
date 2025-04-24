92e5eab035ab881dd0bcbde7fd6d60bf34726762
franwrld <franwrld22@gmail.com>
2025-04-24 02:04:57 -0600

    Actualizar Proyecto API terminado

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,7 +4,7 @@ import { ProjectController } from '../controllers/ProjectController'
 import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
-
+// Crear Proyecto
 router.post('/',
     body('projectName').notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
     body('clientName').notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
@@ -12,13 +12,22 @@ router.post('/',
     handleInputErrors,
     ProjectController.createProject
 )
-
+// Obtener todos los proyctos
 router.get('/', ProjectController.getAllProjects)
-
+// Obtener Proyecto por su ID
 router.get('/:id',
     param('id').isMongoId().withMessage('ID no valido'),
     handleInputErrors,
     ProjectController.getProjectById
+)
+// Actualizar/Update
+router.put('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    body('projectName').notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName').notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description').notEmpty().withMessage('La Descripcion es Obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
 )
 
 export default router
