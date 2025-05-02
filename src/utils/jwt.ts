3bc0a1bcc01ec51cc963cc25dfe0cee241765d87
franwrld <franwrld22@gmail.com>
2025-05-01 23:19:33 -0600

    error type en id cambiado a string

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,12 +2,14 @@ import jwt from 'jsonwebtoken'
 import Types from 'mongoose'
 
 type UserPayload = {
-    id: Types.ObjectId
+    id: string;
 }
 
-export const generateJWT = (payload: UserPayload) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '180d'
-    })
-    return token
-}
+export const generateJWT = (payload: UserPayload): string => {
+    const token = jwt.sign(
+        { id: payload.id.toString() }, // Convertimos ObjectId a string
+        process.env.JWT_SECRET!,
+        { expiresIn: '180d' }
+    );
+    return token;
+};
